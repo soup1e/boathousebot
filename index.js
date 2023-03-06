1df7dd2de01f35643c8eaf02343f052eccb37cd2
Sam Adams <sam.adams7785@gmail.com>
2023-03-06 00:13:08 -0800

    working Slash Commands

--- index.js
+++ index.js
@@ -1,10 +1,69 @@
-const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { DisTube } = require("distube");
+const { SpotifyPlugin } = require("@distube/spotify");
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.login(process.env.TOKEN);
+client.distube = new DisTube(client, {
+  leaveOnStop: false,
+  leaveOnFinish: false,
+  emitNewSongOnly: true,
+  emitAddSongWhenCreatingQueue: false,
+  emitAddListWhenCreatingQueue: false,
+  plugins: [new SpotifyPlugin()],
+});
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, () => {
+  console.log("Ready!");
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
+
+client.login(token);
